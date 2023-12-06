a27aee23fde28a68b42ec46073a77a0bb4af2fc9
arnabsinha4u <arnabsinha4u@gmail.com>
2023-12-06 11:11:24 +0100

    Readme updated and in-code documentation done

--- hello-world-k8s/deploy-e2e-hello-world.sh
+++ hello-world-k8s/deploy-e2e-hello-world.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+#Deploy Ingress controller Nginx
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/cloud/deploy.yaml
+
+#Deploy Hello World App and Service
 kubectl apply -f hello-world-ingress.yaml
+
 echo "Waiting for external IP of ingress and LB"
 sleep 60
+
+#Configure the ingress controller
 kubectl apply -f ingress.yaml
+
+#Retrieve deployed details
 kubectl get svc --all-namespaces
 kubectl get svc -n ingress-nginx
 kubectl get po
--- hello-world-k8s/remove-e2e-hello-world.sh
+++ hello-world-k8s/remove-e2e-hello-world.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#Remove Ingress Controller configuration
 kubectl delete -f ingress.yaml
+
+#Remove Hello World app and Service
 kubectl delete -f hello-world-ingress.yaml
+
+#Remove Ingress Controller
 kubectl delete -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/cloud/deploy.yaml
